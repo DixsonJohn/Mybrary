a26ec5dbed2e1fdf38dc04526f3ab0103039bb1e
DixsonJohn <dixson.nj@gmail.com>
2022-07-02 19:03:12 +0530

    Books New Books

--- routes/author.js
+++ routes/author.js
@@ -31,7 +31,7 @@ router.post('/',async (req,res)=>{
     })
     try{
         const newAuthor = await author.save()
-        //res.redirect(`author/${newAuthor}`)
+        //res.redirect(`author/${newAuthor.id}`)
         res.redirect(`author`)
     }catch{
         res.render('author/new', {
